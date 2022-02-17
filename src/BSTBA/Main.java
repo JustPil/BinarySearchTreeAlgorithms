718ba105327bebc5b77040c14c7be6fca377936f
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-17 10:31:38 -0800

    Created a test driver method

--- src/BSTBA/Main.java
+++ src/BSTBA/Main.java
@@ -7,63 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        String choice = "";
-        while(!choice.equalsIgnoreCase("X")) {
-            menu();
-            choice = scn.nextLine();
-            if(choice.equals("1")) {
-                System.out.print("Input an integer to insert into the BST: ");
-                int input = scn.nextInt();
-                scn.nextLine();
-                System.out.println("Added: " + bst.insert(input));
-            } else if(choice.equals("2")) {
-                System.out.print("Input an integer to remove from the BST: ");
-                int input = scn.nextInt();
-                scn.nextLine();
-                System.out.println("Removed: " + bst.remove(input));
-            } else if(choice.equals("3")) {
-                System.out.print("Input an integer to search for in the BST: ");
-                int input = scn.nextInt();
-                scn.nextLine();
-                System.out.println("Found: " + bst.search(input));
-            } else if(choice.equals("4")) {
-                System.out.println("Full tree: " + bst.isFull());
-            } else if(choice.equals("5")) {
-                System.out.println("Empty tree: " + bst.isEmpty());
-            } else if(choice.equals("6")) {
-                System.out.println("Size: " + bst.size());
-            } else if(choice.equals("7")) {
-                System.out.println("Height: " + bst.height());
-            } else if(choice.equals("8")) {
-                System.out.println("Smallest node: " + bst.minNode());
-            } else if(choice.equals("9")) {
-                System.out.println("Largest node: " + bst.maxNode());
-            } else if(choice.equals("10")) {
-                System.out.println("Number of leaf nodes: " + bst.leafCounter());
-            } else if(choice.equals("11")) {
-                System.out.println("Number of only child nodes: " + bst.onlyChild());
-            } else if(choice.equals("12")) {
-                System.out.println("Inorder traversal: " +
-                        bst.inOrder());
-            } else if(choice.equals("13")) {
-                System.out.println("Preorder traversal: " + bst.preOrder());
-            } else if(choice.equals("14")) {
-                System.out.println("Postorder traversal: " + bst.postOrder());
-            } else if(choice.equals("15")) {
-                System.out.println("Levelorder traversal: " + bst.levelOrder());
-            } else if(choice.equals("16")) {
-                bst.setRoot(bst.balance(0, bst.size() - 1));
-                System.out.println("The tree has been balanced");
-            } else if(choice.equals("17")) {
-                System.out.println("Fullness ratio is " + bst.fullnessRatio());
-            } else if(choice.equals("18")) {
-                System.out.println("Boundary traversal: " + bst.boundaryOrder());
-            } else if(choice.equalsIgnoreCase("X")) {
-                break;
-            } else {
-                System.out.println("Invalid choice.");
-            }
-        }
+        testDriver(scn, bst);
+        scn.close();
     }
 
     /**
@@ -82,5 +27,70 @@ public class Main {
                 "traversal of the BST\n16 - BALANCE - Balances the BST\n17 - FULLNESS RATIO - Returns the ratio " +
                 "between the BST's minimum height and it's current height\n18 - BOUNDARY TRAVERSAL - Performs " +
                 "boundar traversal of the BST\nX - Terminate\n\nEnter choice: ");
+    }
+
+    /**
+     * testDriver A test driver for interacting with a BinarySearchTree object.
+     * @param scn A Scanner object for user input.
+     * @param bst A BinarySearchTree object.
+     */
+    public static void testDriver(Scanner scn, BinarySearchTree<Integer> bst) {
+        String choice = "";
+        while(!choice.equalsIgnoreCase("X")) {
+            menu();
+            choice = scn.nextLine();
+            if (choice.equals("1")) {
+                System.out.print("Input an integer to insert into the BST: ");
+                int input = scn.nextInt();
+                scn.nextLine();
+                System.out.println("Added: " + bst.insert(input));
+            } else if (choice.equals("2")) {
+                System.out.print("Input an integer to remove from the BST: ");
+                int input = scn.nextInt();
+                scn.nextLine();
+                System.out.println("Removed: " + bst.remove(input));
+            } else if (choice.equals("3")) {
+                System.out.print("Input an integer to search for in the BST: ");
+                int input = scn.nextInt();
+                scn.nextLine();
+                System.out.println("Found: " + bst.search(input));
+            } else if (choice.equals("4")) {
+                System.out.println("Full tree: " + bst.isFull());
+            } else if (choice.equals("5")) {
+                System.out.println("Empty tree: " + bst.isEmpty());
+            } else if (choice.equals("6")) {
+                System.out.println("Size: " + bst.size());
+            } else if (choice.equals("7")) {
+                System.out.println("Height: " + bst.height());
+            } else if (choice.equals("8")) {
+                System.out.println("Smallest node: " + bst.minNode());
+            } else if (choice.equals("9")) {
+                System.out.println("Largest node: " + bst.maxNode());
+            } else if (choice.equals("10")) {
+                System.out.println("Number of leaf nodes: " + bst.leafCounter());
+            } else if (choice.equals("11")) {
+                System.out.println("Number of only child nodes: " + bst.onlyChild());
+            } else if (choice.equals("12")) {
+                System.out.println("Inorder traversal: " +
+                        bst.inOrder());
+            } else if (choice.equals("13")) {
+                System.out.println("Preorder traversal: " + bst.preOrder());
+            } else if (choice.equals("14")) {
+                System.out.println("Postorder traversal: " + bst.postOrder());
+            } else if (choice.equals("15")) {
+                System.out.println("Levelorder traversal: " + bst.levelOrder());
+            } else if (choice.equals("16")) {
+                bst.setRoot(bst.balance(0, bst.size() - 1));
+                System.out.println("The tree has been balanced");
+            } else if (choice.equals("17")) {
+                System.out.println("Fullness ratio is " + bst.fullnessRatio());
+            } else if (choice.equals("18")) {
+                System.out.println("Boundary traversal: " + bst.boundaryOrder());
+            } else if (choice.equalsIgnoreCase("X")) {
+                break;
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
     }
 }
